f24a1a280b94b8577a947b75b8397ef989ca77b3
Rafael <rafaelfreire0@gmail.com>
2025-06-16 16:35:09 -0300

    up

--- src/app/settings.ts
+++ src/app/settings.ts
@@ -1,13 +1,11 @@
 import { OpenAI, OpenAIEmbedding } from "@llamaindex/openai";
-import { Ollama, OllamaEmbedding } from '@llamaindex/ollama';
 import { Settings } from "llamaindex";
 
 export function initSettings() {
-  Settings.llm = new Ollama({
-    model: "llama3.2:latest",
+  Settings.llm = new OpenAI({
+    model: "gpt-4.1-mini",
   });
-
-  Settings.embedModel = new OllamaEmbedding({
-    model: "nomic-embed-text"
+  Settings.embedModel = new OpenAIEmbedding({
+    model: "text-embedding-3-small",
   });
 }
