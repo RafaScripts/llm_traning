7b9be2bf8471458c6c8990efacda9fc94704b234
Rafael <rafaelfreire0@gmail.com>
2025-06-16 11:06:29 -0300

    update

--- src/app/settings.ts
+++ src/app/settings.ts
@@ -1,11 +1,13 @@
 import { OpenAI, OpenAIEmbedding } from "@llamaindex/openai";
+import { Ollama, OllamaEmbedding } from '@llamaindex/ollama';
+import { HuggingFaceEmbedding } from '@llamaindex/huggingface';
 import { Settings } from "llamaindex";
 
 export function initSettings() {
-  Settings.llm = new OpenAI({
-    model: "gpt-4.1",
+  Settings.llm = new Ollama({
+    model: "llama3.2:latest",
   });
-  Settings.embedModel = new OpenAIEmbedding({
-    model: "text-embedding-3-small",
-  });
+  Settings.embedModel = new HuggingFaceEmbedding({
+  modelType: "BAAI/bge-small-en-v1.5",
+});
 }
