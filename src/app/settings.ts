7078f4ae98ccbe4bbf2290e688ea7acbefc63042
Rafael <rafaelfreire0@gmail.com>
2025-06-16 15:01:23 -0300

    up

--- src/app/settings.ts
+++ src/app/settings.ts
@@ -4,11 +4,12 @@ import { HuggingFaceEmbedding } from '@llamaindex/huggingface';
 import { Settings } from "llamaindex";
 
 export function initSettings() {
-  Settings.llm = new Ollama({
-    model: "llama3.2:latest",
-  });
-
-  Settings.embedModel = new OllamaEmbedding({
-    model: "nomic-embed-text"
+  Settings.llm = new OpenAI(
+    {
+      model: 'gpt-4.1-nano'
+    }
+  );
+  Settings.embedModel = new OpenAIEmbedding({
+    model: 'text-embedding-3-small'
   });
 }
